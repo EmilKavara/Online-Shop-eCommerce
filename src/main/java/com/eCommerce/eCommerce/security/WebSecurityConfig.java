95f90c1385a9bc05b3c3f60c84738a91f1944a09
Amer1982 <75480636+Amer1982@users.noreply.github.com>
2021-12-01 22:53:14 +0100

    Added security

--- src/main/java/com/eCommerce/eCommerce/security/WebSecurityConfig.java
+++ src/main/java/com/eCommerce/eCommerce/security/WebSecurityConfig.java
@@ -18,31 +18,42 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
-				.and()
-			.formLogin()
-				.loginPage("/login")
-				.permitAll()
-				.and()
-			.logout()
-				.permitAll();
-	}
 
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
 
-		return new InMemoryUserDetailsManager(user);
-	}
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user
+                = User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+                
+                
+
+
+    
+        UserDetails admin
+                = User.withDefaultPasswordEncoder()
+                        .username("admin")
+                        .password("password")
+                        .roles("ADMIN")
+                        .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
 }
