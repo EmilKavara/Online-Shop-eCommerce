97d2a1d0a13e92f1103cf8d2f23a66d203d0809a
Amer1982 <75480636+Amer1982@users.noreply.github.com>
2021-12-07 18:16:35 +0100

    Cleaned the code

--- src/main/java/com/eCommerce/eCommerce/security/WebSecurityConfig.java
+++ src/main/java/com/eCommerce/eCommerce/security/WebSecurityConfig.java
@@ -19,59 +19,37 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+
     @Autowired
-    public WebSecurityConfig(PasswordEncoder passwordEncoder){
-        this.passwordEncoder=passwordEncoder;
+    public WebSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
-    
+
     @Autowired
-	AuthenticationSuccessHandler successHandler;
-    
+    AuthenticationSuccessHandler successHandler;
+
     @Override
-	public void configure(AuthenticationManagerBuilder authenticationMgr) throws Exception {
-		authenticationMgr.inMemoryAuthentication()
-			.withUser("user").password(passwordEncoder.encode("password")).roles("USER")
-			.and()
-			.withUser("admin").password(passwordEncoder.encode("password")).roles("ADMIN");
-	}
-    
+    public void configure(AuthenticationManagerBuilder authenticationMgr) throws Exception {
+        authenticationMgr.inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder.encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder.encode("password")).roles("ADMIN");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
-                //.antMatchers("/", "/login").permitAll()
                 .antMatchers("/userPage").hasAnyRole("USER")
                 .antMatchers("/adminPage").hasAnyRole("ADMIN")
                 .and()
                 .formLogin().loginPage("/login")
-                
-                //.usernameParameter("username").passwordParameter("password")
                 .successHandler(successHandler).permitAll()
-                
                 .and()
                 .logout().permitAll()
                 .logoutSuccessUrl("/home");
-       
+
     }
-
-   /*  @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user=User.builder()
-               // = User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password(passwordEncoder.encode("password"))
-                        .roles("USER")
-                        .build();
-
-        UserDetails admin=User.builder()
-                //= User.withDefaultPasswordEncoder()
-                        .username("admin")
-                        .password(passwordEncoder.encode("password"))
-                        .roles("ADMIN")
-                        .build();
-        return new InMemoryUserDetailsManager(user, admin);
-    }*/
 }
