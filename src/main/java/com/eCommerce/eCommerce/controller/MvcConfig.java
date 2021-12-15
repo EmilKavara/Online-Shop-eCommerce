5596c503645f6ab1e67d68c88cd3a7509a4c1b1e
rijad.muhic <rijad.muhic@fba.ba>
2021-12-15 19:03:20 +0100

    Code refactoring

--- src/main/java/com/eCommerce/eCommerce/controller/MvcConfig.java
+++ src/main/java/com/eCommerce/eCommerce/controller/MvcConfig.java
@@ -12,14 +12,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-        @Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/home").setViewName("home");
-		registry.addViewController("/").setViewName("home");
-		registry.addViewController("/hello").setViewName("hello");
-		registry.addViewController("/login").setViewName("login");
-                registry.addViewController("/userPage").setViewName("userPage");
-                registry.addViewController("/adminPage").setViewName("adminPage");
-	}
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/userPage").setViewName("userPage");
+        registry.addViewController("/adminPage").setViewName("adminPage");
+    }
 
 }
