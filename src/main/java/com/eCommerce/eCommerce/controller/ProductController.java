94b6fbb1b1c90261e9add5e3ad006fa1f8aeedc0
Amer1982 <75480636+Amer1982@users.noreply.github.com>
2021-12-23 14:27:13 +0100

    Working on table buttons

--- src/main/java/com/eCommerce/eCommerce/controller/ProductController.java
+++ src/main/java/com/eCommerce/eCommerce/controller/ProductController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +40,7 @@ public class ProductController {
         model.addAttribute("products", products);
         return "testTable";
     }
-    
+
     @GetMapping("/getproduct")
     private List<Product> getAllProducts() {
         return productService.getAllProducts();
@@ -50,14 +51,21 @@ public class ProductController {
         return productService.getProductById(idproduct);
     }
 
-    @DeleteMapping("/product/{idproduct}")
-    private void deleteProduct(@PathVariable("idproduct") int idproduct) {
+    @RequestMapping(value = "/products/delete/{idproduct}", method = {RequestMethod.GET, RequestMethod.DELETE})
+    public String delete(@PathVariable int idproduct) {
         productService.delete(idproduct);
+        return "redirect:/product/products";
     }
 
+    /*@DeleteMapping("/product/{idproduct}")
+    private void deleteProduct(@PathVariable("idproduct") int idproduct) {
+        productService.delete(idproduct);
+    }*/
+
     @PostMapping(path = "/add")
-    public @ResponseBody String addNewProduct(
-            String name, @RequestParam String description, 
+    public @ResponseBody
+    String addNewProduct(
+            String name, @RequestParam String description,
             @RequestParam BigDecimal price, @RequestParam int quantity) {
 
         Product product = new Product();
@@ -72,7 +80,7 @@ public class ProductController {
         Discount discount = new Discount();
         discount.setIddiscount(1);
         product.setDiscountId(discount);
-        
+
         ProductCategory category = new ProductCategory();
         category.setIdproductCategory(1);
         product.setCategoryId(category);
